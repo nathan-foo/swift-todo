c722be8e491cd1d48d1098f53583a8e02b97b3e0
Nathan Foo <ntfoo27@outlook.com>
2025-02-06 16:52:41 -0600

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Nathan Foo on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TasksTests/TasksTests.swift
@@ -0,0 +1,17 @@
+//
+//  TasksTests.swift
+//  TasksTests
+//
+//  Created by Nathan Foo on 2/6/25.
+//
+
+import Testing
+@testable import Tasks
+
+struct TasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
